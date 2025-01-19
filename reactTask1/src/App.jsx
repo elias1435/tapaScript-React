05306e3452c9f32c0647c98e974a489ba6892df4
elias1435 <elias1435@gmail.com>
2025-01-19 18:28:59 +0600

    first commit

---
+++ reactTask1/src/App.jsx
@@ -0,0 +1,11 @@
+import Banner from './Banner'
+
+function App() {
+  return (
+    <>
+      <Banner />
+    </>
+  )
+}
+
+export default App
---
+++ reactTask1/src/Banner.jsx
@@ -0,0 +1,8 @@
+
+function Banner() {
+  return (
+    <div className="text-xl text-green-400 text-center p-4 bg-black">We are there to learn React from tapaScript</div>
+  )
+}
+
+export default Banner
